fe6df7cd6b6fe0fe713bf702a2ef5f3e81541354
khg <winquake3@hanmail.net>
2025-04-02 22:41:36 +0900

    readme update + 파일 정리

--- ch1/1_0_hello_world/target/debug/deps/hello-03d69c40619239ee.d
+++
@@ -1,5 +0,0 @@
-D:\khg_EX\Rust-Linux-Kernel\ch1\1_0_hello_world\target\debug\deps\libhello-03d69c40619239ee.rmeta: src\main.rs
-
-D:\khg_EX\Rust-Linux-Kernel\ch1\1_0_hello_world\target\debug\deps\hello-03d69c40619239ee.d: src\main.rs
-
-src\main.rs:
