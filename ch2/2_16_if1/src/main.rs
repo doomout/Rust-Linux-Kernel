4e8adbf97caa8126ded1e156821a8bc32e15476d
khg <winquake3@hanmail.net>
2025-04-04 00:52:28 +0900

    if 문 설명

--- ch2/2_16_if1/src/main.rs
+++ ch2/2_16_if1/src/main.rs
@@ -1,8 +1,14 @@
+// Rust의 if문
 fn main() {
     let condition = true;
+
+    //()가 없는 것이 특징이다.
     if condition == true {
         println!("조건이 참 입니다.");
     } else {
         println!("조건이 거짓 입니다.");
     }
 }
+/*실행결과
+조건이 참 입니다.
+ */
--- ch2/2_17_if2/src/main.rs
+++ ch2/2_17_if2/src/main.rs
@@ -1,9 +1,19 @@
+// let if 예제
+// if문은 조건에 따라 다른 값을 반환할 수 있습니다.
+
 fn main() {
     let condition = true;
+
+    // if문의 값을 ret에 저장
     let ret = if condition == true {
-        String::from("조건이 참 입니다.") // ;을 붙이면 컴파일 오류 발생합니다.
+        String::from("조건이 참 입니다.") // ;을 붙이면 컴파일 오류 발생
     } else {
-        String::from("조건이 거짓 입니다.") // ;을 붙이면 컴파일 오류 발생합니다.
-    }; // 여기는 ;을 붙이셔야 합니다.
+        String::from("조건이 거짓 입니다.") // ;을 붙이면 컴파일 오류 발생
+    }; // 여기는 ;을 붙여야 한다.
+    
     println!("ret={}", ret);
 }
+
+/*실행결과
+ret=조건이 참 입니다.
+*/
