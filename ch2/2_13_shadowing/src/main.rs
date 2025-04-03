7e1779e6d9de0313cbce1966005dc6efcf84a36d
khg <winquake3@hanmail.net>
2025-04-04 00:26:43 +0900

    섀도잉 설명 추가

--- ch2/2_13_shadowing/src/main.rs
+++ ch2/2_13_shadowing/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
     let var = 1;
     println!("var={}", var);
-    let var = var + 1; // var를 쉐도잉 처리
+    //var = var + 1; // 컴파일 오류 발생
+    let var = var + 1; // 기존의 var는 소멸되고 var이라는 새로운 변수가 생성(이것을 shadowing이라고 함)
     println!("var={}", var);
 }
+
+/*실행 결과
+var=1
+var=2
+*/
--- ch2/2_15_shadowing2/src/main.rs
+++ ch2/2_15_shadowing2/src/main.rs
@@ -1,6 +1,14 @@
+// 다른 자료형으로 섀도잉
+// Rust에서는 같은 이름의 변수를 재정의할 수 있다.
+// 이때, 기존 변수는 사라지고 새로운 변수가 생성된다.
+// 이 과정을 섀도잉(shadowing)이라고 한다.
 fn main() {
     let var = 1; 
     println!("var={}", var);
     let var = String::from("기존 var를 쉐도잉");
     println!("var={}", var);
 }
+/*실행 결과
+var=1
+var=기존 var를 쉐도잉
+*/
