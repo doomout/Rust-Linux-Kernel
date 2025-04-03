6190cc6f445171d79538bba3aea3ffa59f7c0932
khg <winquake3@hanmail.net>
2025-04-04 01:55:16 +0900

    match 설명 추가

--- ch2/2_18_match1/src/main.rs
+++ ch2/2_18_match1/src/main.rs
@@ -1,8 +1,14 @@
+
+// match 문: 하나의 조건으로 여러 구문을 분기
+// switch 문과 같다.
 fn main() {
     let var = 1;
     match var {
         1 => println!("하나"),
         2 => println!("둘"),
-        _ => println!("기타"),
+        _ => println!("기타"), // default 조건
     }
 }
+/*실행결과
+하나
+ */
--- ch2/2_19_match2/src/main.rs
+++ ch2/2_19_match2/src/main.rs
@@ -1,3 +1,8 @@
+;//let- mathc 문
+// match 문: 하나의 조건으로 여러 구문을 분기
+// switch 문과 같다.
+// match 문은 조건에 따라 여러 구문을 분기할 수 있습니다.
+// match 문은 switch 문과 비슷합니다.
 fn main() {
     let var = 1;
     let ret = match var {
@@ -5,5 +10,10 @@ fn main() {
         2 => String::from("둘"),
         _ => String::from("기타"),
     }; // 세미콜론을 붙여야 합니다.
+    
     println!("ret={}", ret);
 }
+
+/*실행결과
+ret=하나
+*/
