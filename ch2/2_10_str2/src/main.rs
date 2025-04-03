00d0e05408ccfd98d16d751ca61e9c699f00c0e7
khg <winquake3@hanmail.net>
2025-04-03 23:45:54 +0900

    문자열 예제 설명 추가

--- ch2/2_10_str2/src/main.rs
+++ ch2/2_10_str2/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
     let s: &str = "  Hello Rust ";  
-    println!("{}", s.trim());  
-    println!("{}", s.to_lowercase());
-    println!("{}", s.to_uppercase());
+    println!("{}", s.trim());  // 앞뒤 공백제거
+    println!("{}", s.to_lowercase()); //소문자로 변환
+    println!("{}", s.to_uppercase()); //대문자로 변환
 }
+
+/**실행결과
+ Hello Rust
+  hello rust 
+  HELLO RUST 
+ */
--- ch2/2_11_string1/src/main.rs
+++ ch2/2_11_string1/src/main.rs
@@ -1,6 +1,17 @@
+// String 타입을 사용하여 문자열을 생성하고 수정하는 예제
+// String 타입은 가변적이며, 문자열을 동적으로 생성하고 수정할 수 있습니다.
+// String 타입은 Rust의 표준 라이브러리에서 제공하는 문자열 타입으로, 가변적이고 동적으로 크기를 조절할 수 있습니다.
+// String 타입은 힙에 저장되며, 문자열을 조작하는 다양한 메서드를 제공합니다.
+// String 타입은 Rust의 표준 라이브러리에서 제공하는 문자열 타입으로, 가변적이고 동적으로 크기를 조절할 수 있습니다.
+// String 타입은 힙에 저장되며, 문자열을 조작하는 다양한 메서드를 제공합니다.
 fn main() {
-    let mut s = String::from("Hello"); // Hello로 String생성
+    let mut s = String::from("Hello"); // Hello로 String 생성
     println!("{}", s); 
     s.push_str(" Rust!"); // s뒤에 Rust! 추가
     println!("{}", s);
 }
+
+/** 실행결과
+  Hello
+  Hello Rust!
+*/
