728cf8ede2eb7c61e1d41526b144f240d507536d
khg <winquake3@hanmail.net>
2025-04-07 01:48:19 +0900

    소유권 예제 중 - 빌림 방식

--- ch3/3_3_ownership3/src/main.rs
+++ ch3/3_3_ownership3/src/main.rs
@@ -1,10 +1,16 @@
+//함수 파라미터로 값을 전달하는 경우 소유권도 함께 이전 되기에
+// 변수의 재사용이 어렵다.
+
 fn main() {
     let s = String::from("Hello");
-    let s = push_str(s); // push_str에 ownership을 전달하고, 쉐도잉방식으로 s를 획득
-    println!("{}", s);
+    push_str(s); // push_str 함수에 소유권을 전달 
+    println!("{}", s); // s를 사용하는 순간 컴파일 오류 발생
 }
 
-fn push_str(mut s: String) -> String {
+fn push_str(mut s: String) {
     s.push_str(" Rust!");
-    s
 }
+
+/* 실행 결과
+error: aborting due to 1 previous error
+*/
--- ch3/3_4_borrow1/src/main.rs
+++ ch3/3_4_borrow1/src/main.rs
@@ -1,7 +1,9 @@
+// 빌림을 사용해 소유권 공유
+// 값은 전달하되 소유권은 유지하고 싶을 경우 사용
 fn main() {
     let mut s = String::from("Hello");
 
-    // s의 소유권을 push_str에 대여
+    //s 의 소유권을 이전하지 않고, 참조를 전달해 문자열의 내용을 변경
     push_str(&mut s);
 
     // s는 소유권을 유지하고 있기에 정상 동작
@@ -11,3 +13,7 @@ fn main() {
 fn push_str(s: &mut String) {
     s.push_str(" Rust!");
 }
+
+/*실행결과
+Hello Rust!
+*/
