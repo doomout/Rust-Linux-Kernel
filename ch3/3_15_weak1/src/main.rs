0c8fa4bb46d49e0ca67a9022480d6dbc5f501a21
khg <winquake3@hanmail.net>
2025-04-08 06:15:16 +0900

    동정 메모리 할당: Weak 를 사용하여 약한 참조

--- ch3/3_15_weak1/src/main.rs
+++ ch3/3_15_weak1/src/main.rs
@@ -1,41 +1,63 @@
+// Rc와 RefCell을 사용한 순환 참조 메모리 누수 예제
 use std::rc::Rc;
 use std::cell::RefCell;
 
+// Person 구조체 정의
 struct Person {
-    id: i32,
-    next: RefCell<Option<Rc<Person>>>,
+    id: i32,                        // Person 식별자 (id)
+    next: RefCell<Option<Rc<Person>>>, // 다음 사람을 가리키는 참조
 }
 
+// Rc 참조카운트가 0이 되면 자동 호출되는 drop 함수 구현
 impl Drop for Person {
     fn drop(&mut self) {
-        println!("p{} Drop!", self.id); // p1과 p2는 drop이 불리지 않음
+        // drop이 호출되면 어떤 Person이 해제되는지 출력
+        println!("p{} Drop!", self.id);
     }
 }
 
 fn main() {
+    // p1 객체 생성 (Rc로 감싸서 참조 카운트 관리)
     let mut p1 = Rc::new(Person {
         id: 1,
-        next: RefCell::new(None),
+        next: RefCell::new(None),   // next는 None으로 초기화
     });
 
+    // p2 객체 생성
     let mut p2 = Rc::new(Person {
         id: 2,
         next: RefCell::new(None),
     });
 
+    // p3 객체 생성
     let mut p3 = Rc::new(Person {
         id: 3,
         next: RefCell::new(None),
     });
 
-    let mut next = p1.next.borrow_mut();
-    *next = Some(p2.clone()); // p1뒤에 p2를 추가
-    
-    let mut next = p2.next.borrow_mut();
-    *next = Some(p1.clone()); // p2뒤에 p1를 추가
+    {
+        let mut next = p1.next.borrow_mut(); // RefCell을 사용해 내부 가변 접근
+        *next = Some(p2.clone());           // p1 뒤에 p2 추가
+    }
 
-    println!("p1 RefCount: {} p2: RefCount: {}", 
+    {
+        let mut next = p2.next.borrow_mut();
+        *next = Some(p1.clone());           // p2 뒤에 p1 추가
+    }
+
+    // p1, p2의 참조 카운트 출력
+    println!("p1 RefCount: {} p2 RefCount: {}", 
         Rc::strong_count(&p1), Rc::strong_count(&p2));
-
-    // p3는 Drop이 불리는데 p1과 p2는 Drop이 불리지 않음
+    // 출력 예시
+    // -> Rc 참조카운트가 0이 되어야 Drop 호출됨
+    // -> 하지만 p1 <-> p2가 서로를 참조(순환 참조)하고 있어서
+    // Rc 참조카운트가 0이 되지 않음
+    // -> Drop 함수 호출 안 됨 (메모리 누수 발생)
+ㅠㅒ
+    // 반면 p3는 순환 참조 없이 main 스코프 끝나면 Drop 호출됨
 }
+
+/*실행결과 
+p1 RefCount: 2 p2 RefCount: 2
+p3 Drop! 
+*/
--- ch3/3_16_weak2/src/main.rs
+++ ch3/3_16_weak2/src/main.rs
@@ -1,34 +1,66 @@
-use std::rc::{Rc, Weak};
-use std::cell::RefCell;
+// Rc와 Weak를 사용해서 순환 참조 문제를 해결하는 예제
+use std::rc::{Rc, Weak};  // Rc: 강한 참조 / Weak: 약한 참조
+use std::cell::RefCell;   // 내부 가변성을 위해 사용 (가변 borrow)
 
 struct Person {
-    id: i32,
-    next: RefCell<Option<Weak<Person>>>,
+    id: i32,                          // Person 식별자 (id)
+    next: RefCell<Option<Weak<Person>>>, // 다음 사람 참조 (약한 참조)
+    // Weak<T> 를 사용하면 참조카운트를 증가시키지 않음
 }
 
+// Rc 참조카운트가 0이 되면 자동 호출되는 drop 함수 구현
 impl Drop for Person {
     fn drop(&mut self) {
+        // 메모리 해제될 때 어떤 Person이 drop 되었는지 출력
         println!("p{} Drop!", self.id);
     }
 }
 
 fn main() {
+    // p1 객체 생성 (참조카운트 strong=1)
     let mut p1 = Rc::new(Person {
         id: 1,
         next: RefCell::new(None),
     });
 
+    // p2 객체 생성 (참조카운트 strong=1)
     let mut p2 = Rc::new(Person {
         id: 2,
         next: RefCell::new(None),
     });
 
-    let mut next = p1.next.borrow_mut();
-    *next = Some(Rc::downgrade(&p2)); // weak 방식으로 p2 추가
-    
-    let mut next = p2.next.borrow_mut();
-    *next = Some(Rc::downgrade(&p1)); // weak 방식으로 p1 추가
+    // p1 -> p2 연결 (약한 참조로 연결)
+    {
+        let mut next = p1.next.borrow_mut();        // RefCell을 사용해 내부 가변 접근
+        *next = Some(Rc::downgrade(&p2));           // Rc::downgrade() : Weak 참조 생성
+        // Rc::downgrade를 사용하면 참조카운트 증가하지 않음 (순환참조 해결)
+    }
 
+    // p2 -> p1 연결 (약한 참조로 연결)
+    {
+        let mut next = p2.next.borrow_mut();
+        *next = Some(Rc::downgrade(&p1));           // 약한 참조로 연결
+    }
+
+    // 현재 Rc 참조카운트 출력
     println!("p1 RefCount: {} p2: RefCount: {}", 
         Rc::strong_count(&p1), Rc::strong_count(&p2));
+    // 출력 예시
+    // p1 RefCount: 1 p2 RefCount: 1
+
+    // 약한 참조는 strong_count 증가 안되므로
+    // main 스코프 종료시 Rc 카운트 1 -> 0 으로 떨어짐
+    // -> Drop 호출됨
 }
+
+/* 실행결과
+p1 RefCount: 1 p2 RefCount: 1
+p2 Drop!
+p1 Drop!
+*/
+
+/*
+종류                | 	참조 카운트 증가 여부 |	특징           |	주 용도
+Rc<T> (강한 참조)   |	참조카운트 증가	      | 데이터 소유    |	일반적인 참조
+Weak<T> (약한 참조) |	참조카운트 증가 안함  |	데이터 소유 X  |	순환참조 방지
+*/
