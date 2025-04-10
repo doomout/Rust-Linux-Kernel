7c348a99b33a581ec476ffcb1f6d737cbe4d3a80
khg <winquake3@hanmail.net>
2025-04-10 23:48:06 +0900

    모듈 예제 설명

--- ch4/4_3_module_test/src/lib.rs
+++ ch4/4_3_module_test/src/lib.rs
@@ -1,3 +1,5 @@
+//pub 는 public 이라는 뜻
+// 표현 계층
 pub mod presentation {
     pub mod view {
         pub fn render() {
@@ -6,6 +8,7 @@ pub mod presentation {
     }
 }
 
+//비즈니스 계층
 pub mod business {
     pub mod user {
         pub fn create() {
@@ -14,6 +17,7 @@ pub mod business {
     }
 }
 
+//데이터베이스 계층
 pub mod database {
     pub mod user_dao {
         pub fn create() {
@@ -28,3 +32,16 @@ fn it_wors() {
     business::user::create();
     database::user_dao::create();
 }
+
+/*테스트 결과
+running 1 test
+test it_wors ... ok
+
+test result: ok. 1 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s
+
+   Doc-tests module_test
+
+running 0 tests
+
+test result: ok. 0 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s
+*/
