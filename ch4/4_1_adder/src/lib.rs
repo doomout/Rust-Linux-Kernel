58585ebfdfd6e14f9ee8c75555471eecd7075b3b
khg <winquake3@hanmail.net>
2025-04-10 23:06:21 +0900

    간단한 모듈 만들고 사용해보기

--- ch4/4_1_adder/src/lib.rs
+++ ch4/4_1_adder/src/lib.rs
@@ -1,14 +1,27 @@
+//cargo new adder --lib 로 생성한 lib.rs
+//main 함수가 없고 test 코드가 생성 되었다.
+// cargo test 로 테스트 실행 해야 한다.
+
+// add 함수는 두 개의 usize 타입의 인수를 받아 그 합을 반환
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+// #[cfg(test)]는 이어지는 코드가 테스트 환경에서만 컴파일되도록 지시
 #[cfg(test)]
 mod tests {
+    // 상위 스코프의 모든 항목을 현재 테스트 모듈로 가져온다.
     use super::*;
 
+    // 테스트 함수다.
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let result = add(2, 2); //add 함수에 2, 2를 인수로 전달
+        assert_eq!(result, 4); //결과가 4와 같은지 확인
     }
 }
+
+/*실행 결과
+running 1 test
+test tests::it_works ... ok 
+*/
--- ch4/4_2_calc/src/main.rs
+++ ch4/4_2_calc/src/main.rs
@@ -1,6 +1,14 @@
+// Cargo.toml 에 아래와 같은 의존성 추가
+//[dependencies]
+//adder = { path = "../4_1_adder" }
+
 extern crate adder;
 
 fn main() {
     let ret = adder::add(1, 2);
     println!("1+2={}", ret);
 }
+
+/*실행결과
+1+2=3 
+*/
