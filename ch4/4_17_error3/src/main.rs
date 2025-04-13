6c7fdf3d31961957dbffcb1265e8f0595b339422
khg <winquake3@hanmail.net>
2025-04-13 18:21:24 +0900

    복구 불가능한 오류 예제 설명 추가

--- ch4/4_17_error3/src/main.rs
+++ ch4/4_17_error3/src/main.rs
@@ -1,3 +1,5 @@
+// 복구 불가능한 오류 에제
+// 다양한 실행법으로 오류 표시 가능
 fn div(a: i32, b: i32) -> i32 {
     a / b
 }
@@ -6,3 +8,31 @@ fn main() {
     let ret = div(1, 0);
     println!("ret: {}", ret);
 }
+
+/* vscode run 실행시
+thread 'main' panicked at main.rs:4:5:
+attempt to divide by zero
+note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace 
+*/
+
+/*터미널(PowerShell)에서 $env:RUST_BACKTRACE=1; cargo run 실행시
+thread 'main' panicked at src\main.rs:4:5:
+attempt to divide by zero
+stack backtrace:
+   0: std::panicking::begin_panic_handler
+             at /rustc/4eb161250e340c8f48f66e2b929ef4a5bed7c181/library\std\src\panicking.rs:692
+   1: core::panicking::panic_fmt
+             at /rustc/4eb161250e340c8f48f66e2b929ef4a5bed7c181/library\core\src\panicking.rs:75
+   2: core::panicking::panic_const::panic_const_div_by_zero
+             at /rustc/4eb161250e340c8f48f66e2b929ef4a5bed7c181/library\core\src\panicking.rs:178
+   3: error3::div
+             at .\src\main.rs:4
+   4: error3::main
+             at .\src\main.rs:8
+   5: core::ops::function::FnOnce::call_once<void (*)(),tuple$<> >
+             at /rustc/4eb161250e340c8f48f66e2b929ef4a5bed7c181\library\core\src\ops\function.rs:250
+   6: core::hint::black_box
+             at /rustc/4eb161250e340c8f48f66e2b929ef4a5bed7c181\library\core\src\hint.rs:475
+note: Some details are omitted, run with `RUST_BACKTRACE=full` for a verbose backtrace.
+ */
+
--- ch4/4_18_error4/src/main.rs
+++ ch4/4_18_error4/src/main.rs
@@ -1,5 +1,7 @@
+// 복구 불가능한 오류 발생
 fn div(a: i32, b: i32) -> i32 {
     if b == 0 {
+        // panic! 는 복구 불가능한 오류를 일으키려고 할 때 쓰인다.
         panic!("0으로 나눌 수 없습니다.")
     }
 
@@ -10,3 +12,8 @@ fn main() {
     let ret = div(1, 0);
     println!("ret: {}", ret);
 }
+
+/*실행 결과
+thread 'main' panicked at main.rs:5:9:
+0으로 나눌 수 없습니다. 
+*/
