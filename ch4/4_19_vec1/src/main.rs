f94db0f5ae0507865b2c66f2bfc8d2907b1bb7c8
khg <winquake3@hanmail.net>
2025-04-13 23:08:39 +0900

    컬렉션 중 벡터 설명 추가

--- ch4/4_19_vec1/src/main.rs
+++ ch4/4_19_vec1/src/main.rs
@@ -1,11 +1,18 @@
+// Vec 란? 크기 조정이 가능한 배열
 fn main() {
-    let mut v: Vec<i32> = vec![];
+    // 빈 i32형 벡터를 생성
+    //let mut v: Vec<i32> = Vec::new(); // 이 방법도 있고
+    let mut v: Vec<i32> = vec![]; // 이 방법도 있다.
 
     for i in 1..10 {
-        v.push(i);
+        v.push(i); // i 값을 벡터 v에 삽입
     }
 
-    for d in v {
+    for d in v { // 벡터 v의 각 요소에 대해 반복
         print!("{},", d);
     }
 }
+
+/* 실행결과
+1,2,3,4,5,6,7,8,9, 
+*/
--- ch4/4_19_vec2/src/main.rs
+++ ch4/4_19_vec2/src/main.rs
@@ -1,7 +1,14 @@
+//벡터 개별 요소 읽는 예제
 fn main() {
-    let v = vec![1, 2, 3];
-    let one = v[0];
-    let two = v.get(1);
+    let v = vec![1, 2, 3]; //i32 타입의 벡터 v를 생성하고 1,2,3으로 설정
+    let one = v[0]; //v의 첫 번째 요소를 one에 할당 
+
+    // get() 함수는 해당 인덱스에 값이 있으면 Some(값)을 반환하고, 없으면 None을 반환한다.
+    let two = v.get(1); //v의 두 번째 요소를 two에 할당, Option 타입을 반환하므로 결과는 some(2) 다.
 
     println!("One: {:?}, Two: {:?}", one, two);
 }
+
+/* 실행결과
+One: 1, Two: Some(2)
+*/
