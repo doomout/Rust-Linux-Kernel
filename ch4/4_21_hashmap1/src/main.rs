30f6444f79d9d3986e447f716b5f8c01fb555395
khg <winquake3@hanmail.net>
2025-04-14 20:36:28 +0900

    해쉬 맵 설명 추가

--- ch4/4_21_hashmap1/src/main.rs
+++ ch4/4_21_hashmap1/src/main.rs
@@ -1,13 +1,26 @@
+// 해시 맵 예제
 use std::collections::HashMap;
 
 fn main() {    
+    // HashMap 생성
     let mut books: HashMap<i32, String> = HashMap::new();
 
+    // HashMap에 값 추가
+    // insert() 메서드를 사용하여 키와 값을 추가
+    // 키는 i32, 값은 String 타입
     books.insert(10, String::from("Rust"));
     books.insert(20, String::from("Java"));
     books.insert(30, String::from("Python"));
 
+    // HashMap 출력
+    // for 루프를 사용하여 HashMap의 모든 키와 값을 출력
     for (key, value) in &books {
         println!("Key: {:?}, Value: {:?}", key, value);
     }
 }
+
+/*실행결과
+Key: 30, Value: "Python"
+Key: 20, Value: "Java"
+Key: 10, Value: "Rust" 
+*/
--- ch4/4_21_hashmap2/src/main.rs
+++ ch4/4_21_hashmap2/src/main.rs
@@ -1,12 +1,21 @@
 use std::collections::HashMap;
 
 fn main() {    
+    // HashMap 생성
     let mut books: HashMap<i32, String> = HashMap::new();
 
+    // HashMap에 값 추가
     books.insert(10, String::from("Rust"));
     books.insert(20, String::from("Java"));
     books.insert(30, String::from("Python"));
 
+    // get() 메서드를 사용하여 키에 해당하는 값을 가져옴
+    // get() 메서드는 Option<T>를 반환하므로 unwrap() 메서드를 사용하여 값을 가져옴
+    // get() 메서드는 키에 해당하는 값이 없으면 None을 반환
     let rust = books.get(&10);
     println!("key 10은 {:?}", rust);
 }
+
+/*실행결과
+key 10은 Some("Rust")
+*/
