77db5fbad6b46d5d50baf5f57412da6d47377f8f
khg <winquake3@hanmail.net>
2025-04-18 02:40:31 +0900

    동기식 입출력 예제 설명

--- ch5/5_14_syncio1/src/main.rs
+++ ch5/5_14_syncio1/src/main.rs
@@ -1,6 +1,9 @@
+// 동기식 입출력 방식의 예제
 use std::fs::File;
 use std::io::{Read, Write};
 
+// 동기식 입출력 방식은 작업이 완료 될 때까지 무한정 기다린다.
+// 가장큰 문제는 입출력이 완전히 완료될 때까지 기다리니 CPU 자원을 비효율적으로 사용한다.
 fn main() {
     let mut file = File::open("input.txt").unwrap();
     let mut contents = String::new();
