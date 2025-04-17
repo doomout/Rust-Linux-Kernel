ef135a87ca99727cb48b8f56e5ed299c10bf95e0
khg <winquake3@hanmail.net>
2025-04-18 02:53:46 +0900

    비동기식 입출력 예제 설명

--- ch5/5_15_asyncio1/src/main.rs
+++ ch5/5_15_asyncio1/src/main.rs
@@ -1,14 +1,32 @@
+// 비동기식 입출력 예제
+// 이 코드는 비동기식으로 처리 해서 메인 스레드를 멈추지 않는다.
+
+/*cargo.toml에 의존성 추가
+[dependencies]
+tokio = { version = "1.25.0", features = ["full"] } 
+*/
 use tokio::fs::File;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 #[tokio::main]
 async fn main() {
-    let mut file = File::open("input.txt").await.unwrap();
+    // 비동기 방식으로 file 핸들러를 얻는다.
+    //input.txt 엔 hello world 가 입력되어 있다.
+    let mut file = File::open("input.txt").await.unwrap(); 
     let mut contents = String::new();
+
+    // 비동기 방식으로 file 읽기
     file.read_to_string(&mut contents).await.unwrap();
 
+    // input.txt의 내용을 출력
     println!("{}", contents);
 
+    //비동기 방식으로 file 생성
     let mut file = File::create("output.txt").await.unwrap();
+    //비동기 방식으로 file 저장 (input.txt에 있는 hello world가 output.txt에 저장된다.)
     file.write_all(contents.as_bytes()).await.unwrap();
 }
+
+/*실행결과
+hello world 
+*/
--- ch5/5_16_asyncio2/src/main.rs
+++ ch5/5_16_asyncio2/src/main.rs
@@ -1,3 +1,8 @@
+// 비동기 방식으로 구현한 이벤트 루프 예제
+/* 의존성 추가
+[dependencies]
+tokio = { version = "1.25.0", features = ["full"] }
+*/
 use tokio::io::{stdin, BufReader, AsyncBufReadExt};
 use tokio::fs::File;
 
@@ -21,3 +26,14 @@ async fn main() {
         };
     }
 }
+
+/*실행결과
+test      
+입력: test
+sss
+입력: sss
+dddd
+입력: dddd
+quit
+입력: quit 
+*/
