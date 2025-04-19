1dac524bf4a6e4c1117351c6aec4901183b04622
khg <winquake3@hanmail.net>
2025-04-19 11:28:31 +0900

    데이터 버퍼링 예제 설명

--- ch5/5_17_buffering1/src/main.rs
+++ ch5/5_17_buffering1/src/main.rs
@@ -1,12 +1,27 @@
+// 데이터 버퍼링 예제
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+/*input.txt 내용
+hello world
+i am ferris!
+haha...
+*/
+
 fn main() {
     let file = File::open("input.txt").unwrap();
+    // BufReader 생성
     let reader = BufReader::new(file);
 
+    // File 을 읽는다.
     for line in reader.lines() {
         let line = line.unwrap();
         println!("{}", line);
     }
 }
+
+/*실행결과
+hello world
+i am ferris!
+haha...  
+ */
