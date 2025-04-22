3dee46f9ff0cc0c47cc841af623204eb35c4c929
khg <winquake3@hanmail.net>
2025-04-22 22:35:00 +0900

    Copy 예제 설명 추가

--- ch6/6_1_copy1/src/main.rs
+++ ch6/6_1_copy1/src/main.rs
@@ -1,3 +1,7 @@
+// Copy 예제 
+// Copy 는  묵시적 복사를 수행하며 바이트 단위의 복제인 깊은 복사를 수행한다.
+// 기본적인 데이터 타입에 대해 별도의 소유권 전달 없이 값을 복사한다.
+// Copy 트레잇을 구현한 타입은 스택에 저장된 값이 복사되며, 힙에 저장된 값은 복사되지 않는다.
 #[derive(Debug, Clone, Copy)]
 struct Point {
     x: i32,
@@ -22,3 +26,8 @@ fn main() {
     println!("{:?}", b); // b에 접근 가능
     println!("{:?}", result);
 }
+/*실행결과
+Point { x: 1, y: 2 }
+Point { x: 3, y: 4 }
+Point { x: 4, y: 6 }
+*/
