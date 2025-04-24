cb4c1e0ba5dbb4de937275d4ce7ac72458ccd8df
khg <winquake3@hanmail.net>
2025-04-24 20:20:59 +0900

    Drop 설명 추가

--- ch6/6_3_drop1/src/main.rs
+++ ch6/6_3_drop1/src/main.rs
@@ -1,8 +1,10 @@
+// 객체가 메모리에서 벗어날 때 수행해야 할 작업 지정
 struct Book {
     title: String,
 }
 
 impl Drop for Book {
+    // Drop트레잇을 구현합니다.
     fn drop(&mut self) {
         println!("Book객체 해제: {}", self.title);
     }
@@ -13,3 +15,7 @@ fn main() {
         let book = Book { title: String::from("러스트") };
     } // book의 Drop트레잇이 자동으로 호출됩니다.
 }
+
+/*실행결과
+Book객체 해제: 러스트 
+*/
