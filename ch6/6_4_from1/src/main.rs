c15de14d6ff6d598fcb7a1d8b37c319afad0883c
khg <winquake3@hanmail.net>
2025-04-24 20:42:26 +0900

    From, Into 예제 설명

--- ch6/6_4_from1/src/main.rs
+++ ch6/6_4_from1/src/main.rs
@@ -1,9 +1,14 @@
+// 러스트는 타입에 관한 엄격한 안정성을 보장한다.
+// 그래서 타입을 변환할 때는 명시적으로 변환을 해줘야 한다.
+// From 은 한 자료형을 다른 자료형으로 변환하는 로직을 정의할 때 사용
+// Into는 From을 통해 변환된 자료형을 사용.
 #[derive(Debug)]
 struct Point {
     x: i32,
     y: i32,
 }
 
+// Point 구조체를 (i32, i32) 튜플로 변환하는 From trait 구현
 impl From<(i32, i32)> for Point {
     fn from(tuple: (i32, i32)) -> Self {
         Point { x: tuple.0, y: tuple.1 }
@@ -23,3 +28,8 @@ fn main() {
 
     println!("tuple.into = {:?}", pt);
 }
+
+/*실행결과
+Point::from = Point { x: 1, y: 2 }
+tuple.into = Point { x: 1, y: 2 } 
+*/
