1a0e1e97fddf6cc2c34bee13d1df608611fd28b1
khg <winquake3@hanmail.net>
2025-04-26 23:40:51 +0900

    시간 연산 예제 추가

--- ch6/6_25_timeformatting2/src/main.rs
+++ ch6/6_25_timeformatting2/src/main.rs
@@ -15,3 +15,9 @@ fn main() {
     let seoul = seoul_offset.from_utc_datetime(&now_utc.naive_utc());
     println!("한국시각: {}", seoul);
 }
+
+/*실행결과
+UTC 시각: 2025-04-26 11:11:39.119701 UTC
+로컬 시각: 2025-04-26 20:11:39.119980900 +09:00
+한국시각: 2025-04-26 20:11:39.119701 +09:00 
+*/
