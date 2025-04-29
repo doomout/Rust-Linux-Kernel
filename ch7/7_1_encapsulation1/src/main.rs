5b823e8ac63d7a01ae494812087b0006b386a7a8
khg <winquake3@hanmail.net>
2025-04-29 22:52:48 +0900

    객체 지향 중 캡슐화 예제

--- ch7/7_1_encapsulation1/src/main.rs
+++ ch7/7_1_encapsulation1/src/main.rs
@@ -1,3 +1,6 @@
+// 캡슐화를 위해 pub 키워드를 사용하는 예제
+// 러스트는 기본적으로는 객체 지향 언어가 아니다.
+// 하지만 객제 지향의 핵심적인 개념을 트레잇과 같은 형태로 객체지향에 가까운 프로그램을 작성할 수 있다.
 pub struct Student {
     id: i32, // private 필드
     pub name: String, // public 필드
@@ -25,3 +28,7 @@ fn main() {
     let student = Student::new(1, String::from("luna"), String::from("luna@email.me"));
     println!("이름: {}", student.get_name());
 }
+
+/*실행결과
+이름: luna 
+*/
