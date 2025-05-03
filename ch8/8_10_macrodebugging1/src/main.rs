340c8d2f424ff097f0b5c8d33febce3cdfcc84dd
khg <winquake3@hanmail.net>
2025-05-03 23:46:26 +0900

    매크로 디버깅 방법 설명 추가

--- ch8/8_10_macrodebugging1/src/main.rs
+++ ch8/8_10_macrodebugging1/src/main.rs
@@ -6,3 +6,26 @@ fn main() {
     let world = S!("World");
     println!("Hello, {}!", world);
 }
+
+/* 매크로 디버깅을 하기 위해서 사전 설치 해야 하는 것
+ rustup toolchain install nightly
+
+그 후에 아래 명령어를 실행하면
+ rustc --pretty=expanded main.rs > main.rs.expanded
+*/
+
+
+/*매크로 디버깅 결과
+#![feature(prelude_import)]
+#![no_std]
+#[prelude_import]
+use ::std::prelude::rust_2015::*;
+#[macro_use]
+extern crate std;
+macro_rules! S { ($e:expr) => {String::from($e)}; }
+
+fn main() {
+    let world = String::from("World");
+    { ::std::io::_print(format_args!("Hello, {0}!\n", world)); };
+} 
+*/
