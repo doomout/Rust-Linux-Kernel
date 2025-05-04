7edef1165d510918f89b6d0d51508c5ce609f331
khg <doomout1@gmail.com>
2025-05-04 16:59:22 +0900

    Bindgen 을 사용해  C코드 호출하기

--- ch9/9_0_bindgen1/build.rs
+++ ch9/9_0_bindgen1/build.rs
@@ -4,17 +4,27 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
+    // 현재 디렉터리를 컴파일러에 라이브러리 검색 경로를 알린다.
     println!("cargo:rustc-link-search=.");
+
+    // hello 라이브러리를 링크한다.
     println!("cargo:rustc-link-lib=hello");
+
+    // c_src/hello.h 파일이 변경되면 다시 빌드하도록 설정한다.
     println!("cargo:rerun-if-changed=c_src/hello.h");
     
+    // 'bindgen'을 사용하여 C 헤더 파일에 대한 Rust 바인딩을 생성한다.
+    // 'bindgen'은 C/C++ 헤더 파일을 Rust 코드로 변환하는 도구이다.
     let bindings = bindgen::Builder::default()
-        .header("c_src/hello.h")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .generate()
+        .header("c_src/hello.h") //바인딩할 헤더 파일 설정
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks)) // Cargo 콜백 등록
+        .generate() //바인딩 생성
         .expect("Unable to generate bindings");
 
+    // 출력 경로를 환경 변수 OUT_DIR에서 가져온다.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    
+    // 생성된 바인딩을 Rust 파일로 저장한다.
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
--- ch9/9_0_bindgen1/src/main.rs
+++ ch9/9_0_bindgen1/src/main.rs
@@ -1,13 +1,36 @@
+/* Cargo.toml 에 의존성 추가
+[build-dependencies]
+bindgen = "0.64.0"
+*/
+
+//C와 호환되는 문자열을 다루기 위한 'CString' 타입을 사용한다.
 use std::ffi::CString;
 
+// 이전 단계에서 생성된 ''bindings.rs' 파일을 포함한다.
 mod bindings {
+    // 'OUT_DIR' 환경 변수를 사용하여 생성된 바인딩 파일을 포함한다.
+    // 'OUT_DIR'은 Cargo가 빌드 아티팩트를 저장하는 디렉터리 경로를 나타낸다.
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
 
 fn main() {
+    // C와 호환되는 문자열을 다루기 위한 'CString' 타입을 사용한다.
+    // 'CString::new'는 Rust 문자열을 C 문자열로 변환한다.
     let c_to_print = CString::new("Hello Rust").expect("CString::new failed");
 
+    // 'unsafe' 블록은 Rust의 안전성 규칙을 우회하는 코드 블록이다.
+    // C 라이브러리의 함수를 호출할 때는 'unsafe' 블록이 필요하다.
+    // 'bindings::hello'는 C 라이브러리에서 정의된 'hello' 함수를 호출한다.
+    // 'c_to_print.as_ptr()'는 C 문자열의 포인터를 가져온다.
+    // 'as_ptr()'는 'CString'의 내부 포인터를 반환한다.
     unsafe {
         bindings::hello(c_to_print.as_ptr());
     }
 }
+
+/*실행 명령어
+LD_LIBRARY_PATH=. cargo run
+
+실행 결과
+from rust: Hello Rust
+*/
