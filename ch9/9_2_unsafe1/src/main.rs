0a2cebbb41fe92d544841017d20bc15b61e3913e
khg <doomout1@gmail.com>
2025-05-05 22:39:52 +0900

    unsafe 사용 예제 설명 추가

--- ch9/9_2_unsafe1/src/main.rs
+++ ch9/9_2_unsafe1/src/main.rs
@@ -1,10 +1,18 @@
+//배열의 메모리를 복사하는 예제
 fn main() {
-    let src = [1, 2, 3];
-    let mut dest = [0; 3];
+    let src = [1, 2, 3]; // 원본 배열
+    let mut dest = [0; 3]; // 대상 배열, 0으로 초기화
 
+    // 안전하지 않은 작업을 수행하기 위해 unsafe 블록을 사용
+    // src 배열의 메모리를 dest 배열로 복사
+    // 이때 복사되는 길이(src.len())는 dest의 크기보다 작거나 같아야 한다.
+    // 그렇지 않으면 메모리 안전성 문제가 발생할 수 있다.
     unsafe {
         std::ptr::copy_nonoverlapping(src.as_ptr(), dest.as_mut_ptr(), src.len());
     }
 
     println!("dest: {:?}", dest);
 }
+
+/*실행결과
+dest: [1, 2, 3]
--- ch9/9_3_unsafe2/src/main.rs
+++ ch9/9_3_unsafe2/src/main.rs
@@ -1,17 +1,35 @@
+// 메모리 주소에 직접 접근 예제
 use std::mem;
 
 fn main() {
+    // unsafe 블록을 사용하여 메모리 할당 및 해제를 수행
+    // libc 크레이트를 사용하여 C 스타일의 메모리 할당 및 해제를 수행
+    // libc 크레이트는 C 라이브러리의 기능을 Rust에서 사용할 수 있도록 해준다.
     unsafe {
+        // libc::malloc은 C에서 메모리를 동적으로 할당하는 함수이다.
         let ptr: *mut i32 = libc::malloc(mem::size_of::<i32>()) as *mut i32;
+
+        // 포인터가 null 인 경우 메모리 할당에 실패한 것이므로 프로그램 종료
         if ptr.is_null() {
             panic!("메모리 할당 실패");
         }
 
         let val: *mut i32 = ptr; // ptr과 동일한 주소를 갖는 변수 생성
+
+        // val 포인터가 가리키는 메모리 위치에 정수 123을 저장
+        // unsafe 블록 내에서만 포인터를 사용하여 메모리에 접근할 수 있다.
+        // unsafe 블록 외부에서는 안전한 방법으로 메모리에 접근해야 한다.
         *val = 123;
 
+        // 포인터를 사용하여 메모리에서 값을 읽어온다.
         println!("ptr={}", *ptr);
 
+        // 메모리 해제를 위해 libc::free 함수를 사용
+        // 메모리 해제를 하지 않으면 메모리 누수가 발생할 수 있다.
         libc::free(ptr as *mut libc::c_void);
     }
 }
+
+/*실행결과
+ptr=123
+*/
